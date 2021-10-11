e48d711a9f4abc62ca3abbec9cdbb85a8c6fc18c
Sandeep <sandeepvermaslg77@gmail.com>
2021-10-11 20:11:41 +0530

    Canera Integration

--- app/src/main/java/com/example/automatedbill/Scan.kt
+++ app/src/main/java/com/example/automatedbill/Scan.kt
@@ -1,88 +1,116 @@
 package com.example.automatedbill
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.camera.core.CameraProvider
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.automatedbill.databinding.ActivityScanBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.File
+import java.lang.reflect.Modifier
+import java.nio.ByteBuffer
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class Scan : AppCompatActivity() {
     private lateinit var binding:ActivityScanBinding
-    private var imagecapture:ImageCapture?=null
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScanBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //camera permission request
-        if(!permissions()){
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
             ActivityCompat.requestPermissions(
-                this,Constants.REQUIRED_PERMISSIONS,
-                Constants.REQUEST_CODE_PERMISSIONS
-            )
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        binding.dashboardbtn.setOnClickListener{
-            startActivity(Intent(this,MainActivity::class.java))
-        }
+        // Set up the listener for take photo button
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    //start camera with permissions
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray)
-    {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode== Constants.REQUEST_CODE_PERMISSIONS){
-            if(permissions()){
-                startCamera()
-            }
-        }
-    }
 
     private fun startCamera() {
-        val cameraProviderFuture=ProcessCameraProvider
-            .getInstance(this)
-        cameraProviderFuture.addListener({
-            val cameraProvider:ProcessCameraProvider=cameraProviderFuture.get()
-            val preview=Preview.Builder()
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
                 .build()
-                .also{ mPreview->
-                    mPreview.setSurfaceProvider(
-                        binding.campreview.surfaceProvider
-                    )
+                .also {
+                    it.setSurfaceProvider(binding.campreview.surfaceProvider)
                 }
-            imagecapture=ImageCapture.Builder()
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build()
+            imageAnalyzer.setAnalyzer(cameraExecutor, ImageAnalysis.Analyzer { image ->
+                val rotationDegrees = image.imageInfo.rotationDegrees
+            })
 
+            // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-            try{
+
+            try {
                 cameraProvider.unbindAll()
+
                 cameraProvider.bindToLifecycle(
-                    this,cameraSelector,preview,imagecapture
-                )
-            }
-            catch (e:Exception){
-                Log.d(Constants.TAG,"Camera Failure",e)
+                    this, cameraSelector, preview, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
 
-        },ContextCompat.getMainExecutor(this))
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    //check for camera permission
-    private fun permissions()=Constants.REQUIRED_PERMISSIONS.all {
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            baseContext,it
-        )==PackageManager.PERMISSION_GRANTED
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
